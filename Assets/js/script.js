06d5494cfb56cca87976ab1d4caea29d1a0fc217
lenny-g <leanne_gallag@live.co.uk>
2021-09-29 21:06:43 +0100

    added comments to the javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -62,26 +62,33 @@ const lowerCaseArray = [
   "z",
 ];
 
+// used a formula for uppercase array
 const upperCase = lowerCaseArray.join("").toUpperCase();
 const upperCaseArray = upperCase.split("");
 
+// This gets the length and what the user decides to include in their password (lowercase/uppercase/special/num)
 const getCriteria = function () {
+  // prompt for user to decide the length of their password
   const x = prompt("Choose Password length");
+  // convert the input from a string into an int
   const passwordLength = parseInt(x, 10);
+  // if the string can not be converted into an int (this creates an error message)
   if (Number.isNaN(passwordLength)) {
     alert("Please enter a valid numerical value");
     return;
   }
-  // if input is between the lengths of 8-128
+  // if the length is not between 8 and 128 characters (this creates an error message)
 
   if (passwordLength > 128 || passwordLength < 8) {
     alert("Password must be between 8-128 characters");
     return;
   }
+  // getting the boolean input for each attribute
   const lowerCase = confirm("Would you like to use lowercase characters?");
   const upperCase = confirm("Would you like to use uppercase characters?");
   const specialChar = confirm("Would you like to use special characters?");
   const numChar = confirm("Would you like to use a number?");
+  // value and key properties of each attribute
   const attributes = {
     passwordLength: passwordLength,
     lowerCase: lowerCase,
@@ -94,10 +101,13 @@ const getCriteria = function () {
 
 const generatePassword = function () {
   const attributes = getCriteria();
+  // creating an empty array for the attributes if selected
   const containerArray = [];
+  // if user has received error message this quits the function
   if (!attributes) {
     return;
   }
+  // if selected the attribute is added to the array
   if (attributes.lowerCase === true) {
     containerArray.push(...lowerCaseArray);
   }
@@ -110,19 +120,24 @@ const generatePassword = function () {
   if (attributes.numChar === true) {
     containerArray.push(...numArray);
   }
+  // if the user has pick none of the above arrays there is an error message
   if (!containerArray.length) {
     alert("You need to choose at least one type of character");
     return;
   }
 
+  //  empty array to fill with users chosen attributes
   const passwordArray = [];
 
+  // this adds another random character to the array until it has reached the users passwords length
   for (let i = 0; i < attributes.passwordLength; i++) {
     const randomIndex = Math.floor(Math.random() * containerArray.length);
     const randomChar = containerArray[randomIndex];
     passwordArray.push(randomChar);
   }
+  // this turns the array into a string
   const password = passwordArray.join("");
+  // creates password
   return password;
 };
 
